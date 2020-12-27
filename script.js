78ba0066b04374becff9bb96f5c3c7ba0dd52f57
Shubham Rathor <rshubham005@users.noreply.github.com>
2020-12-27 23:39:00 +0530

    bfvrsfv

---
+++ script.js
@@ -0,0 +1,22 @@
+var input= document.querySelector("#input-text");
+var btn= document.querySelector("#translate-btn");
+var output= document.querySelector("#translated-text");
+btn.addEventListener("click",translate);
+function translate()
+{
+    var text=input.value;
+    var url= "https://api.funtranslations.com/translate/minion.json"+"?text="+text;
+    fetch(url)
+    .then(
+        function response(response)
+        {
+            return response.JSON;
+        }
+    )
+    .then(
+        function parseJson(JSON)
+        {
+            console.log(JSON)
+        }
+    )
+}
